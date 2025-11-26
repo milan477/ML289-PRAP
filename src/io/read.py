5005e1ca620b4db91dc266e46b7a93e2bf757ca9
milan477 <milanliessens@gmail.com>
2025-11-26 02:30:28 -0800

    LayoutLM clustering KMeans

--- src/io/read.py
+++ src/io/read.py
@@ -3,8 +3,10 @@ from src.config import DATA_DIR
 from pathlib import Path
 import pandas as pd
 from src.schema.dataset import PDF,Page,Dataset
+from pdf2image import convert_from_path
+import pytesseract
 
-def _read_single_pdf(path):
+def _read_single_pdf_pypdf(path: Path) -> PDF:
     reader = PdfReader(path)
     print(f"Reading {path.name} ({len(reader.pages)} pages)")
 
@@ -15,18 +17,36 @@ def _read_single_pdf(path):
 
     return pdf
 
-def read_pdfs(path: Path = DATA_DIR):
-    pdfs = []
-    print(f"Reading {path.name} ({len(pdfs)} pages)")
-    i = 0
-    for i in range(30):
-        pdfs.append(_read_single_pdf(path / f"record{i}.pdf"))
+def _read_single_pdf_pytesseract(path: Path) -> PDF:
+    print(f"Reading {path.name} with pytesseract")
+    pages = convert_from_path(path)
 
-    # for file in path.glob('*.pdf'):
+    page = pages[0]
+    data = pytesseract.image_to_data(page,output_type=pytesseract.Output.DICT)
+
+
+    return page, data
+
+def _read_single_pdf(path: Path, method: str = 'pypdf'):
+    if method == 'pypdf':
+        return _read_single_pdf_pypdf(path)
+
+    if method == 'pytesseract':
+        return _read_single_pdf_pytesseract(path)
+    else:
+        raise ValueError(f"Unknown method {method} for reading PDFs")
+
+        # for file in path.glob('*.pdf'):
     #     pdfs.append(_read_single_pdf(file))
     #     if i == 1: break
     #     i += 1
-    return Dataset(pdfs)
+def read_pdfs(path: Path = DATA_DIR, method: str = 'pypdf') -> Dataset:
 
+    pdfs = []
+    print(f"Reading {path.name} ({len(pdfs)} pages) using {method}")
+    i = 0
+    for i in range(2):
+        pdfs.append(_read_single_pdf(path / f"record{i}.pdf", method=method))
+    return Dataset(pdfs)
 if __name__ == '__main__':
-    print(len(read_pdfs()))
+    print(f'read {len(read_pdfs())} PDFs')
